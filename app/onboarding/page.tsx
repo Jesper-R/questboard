31a0e7cf3ce64a38b0fe496b9ecb690d6bb07605
Jesper <Jesperrudegran@gmail.com>
2025-09-17 01:50:24 +0200

    QUES-65 onboarding route + unicorn-studio background

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import UnicornBackground from "@/components/UnicornStudioBackground";
+
+const page = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <UnicornBackground />
+    </div>
+  );
+};
+
+export default page;
