2f8e9285ef98e23ec27a4b6ec45cfc70aafd3274
Jesper-R <jesperrudegran@gmail.com>
2025-09-22 00:41:30 +0200

    QUES-67 added ability to protect routes, added /dashboard to be protected

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
